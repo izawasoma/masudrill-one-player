bf149c9f4d2e2ea6e65e6893cb3bc428159b0345
izawasoma <izawasoma@gmail.com>
2022-05-16 10:34:01 +0900

    aaa

--- routes/game.js
+++ routes/game.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/game', function(req, res, next) {
+  console.log("＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃")
+  console.log("▼gameへの接続を確認しました！")
+  console.log("＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃")
   res.render('game');
 });
 
