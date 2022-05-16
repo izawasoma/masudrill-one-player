2bd8f526d667cd55473f72feaca8080b12b61525
izawasoma <izawasoma@gmail.com>
2022-05-16 13:20:31 +0900

    config書き換え

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 //データベース
 const mysql = require('mysql2');
 const db_config = {
-    host: 'localhost',
+    /* host: 'localhost',
     user: 'root',
     password: '',
-    database: 'masudrill',
-    /* host: 'us-cdbr-east-05.cleardb.net',
+    database: 'masudrill', */
+    host: 'us-cdbr-east-05.cleardb.net',
     user: 'b1d0824a5f80de',
     password: '9555ba80',
-    database: 'heroku_cc6fa88888055ee', */
+    database: 'heroku_cc6fa88888055ee',
 };
 
 var pool = mysql.createPool(db_config);
